a074671c245220cd8e6aaad0b6e38eed94bf7683
salikov <iesalikov@gmail.com>
2022-06-25 18:46:35 +0500

    task 2

--- homework 5/hw_5_2.py
+++ homework 5/hw_5_2.py
@@ -2,3 +2,10 @@
 2. Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 '''
+#from itertools import count
+
+with open('hw_2.txt', 'r') as f:
+    s = f.readlines()
+    print(len(s))
+    for i in range(len(s)):
+        print(f'В строке №{i+1} {len(s[i])} символов')
