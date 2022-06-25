64cb3506e8ab97030f8f233c3b4bb8feaccbd7f9
salikov <iesalikov@gmail.com>
2022-06-25 19:35:36 +0500

    task 3

--- homework 5/hw_5_3.py
+++ homework 5/hw_5_3.py
@@ -3,3 +3,24 @@
 Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
 Выполнить подсчет средней величины дохода сотрудников.
 '''
+
+
+with open('hw_3.txt', 'w') as f:
+    while True:
+        s = input()
+        if s != '':
+            f.write(s + '\n')
+        else:
+            break
+
+with open('hw_3.txt', 'r') as f:
+    file_strings = f.readlines()
+    #print(file_strings)
+    salary_summ = 0
+    for i in range(len(file_strings)):
+        s = file_strings[i].split()
+        if int(s[1]) < 20000:
+            print(s[0])
+        salary_summ += int(s[1])
+        mean_salary = salary_summ / len(file_strings)
+    print(mean_salary)
